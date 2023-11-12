2a4b7a15eea7e892242ca119068beeee12a6392d
root <root@DESKTOP-TJHP05G.localdomain>
2023-11-12 16:39:58 -0300

    Initial setup

---
+++ client/build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+    id("java")
+}
+
+group = "unioeste.sd"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("io.github.spair:imgui-java-app:1.86.11")
+    implementation(project(":remote-lib"))
+    testImplementation(platform("org.junit:junit-bom:5.9.1"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,4 @@
+rootProject.name = "voting-rmi-app"
+include("server")
+include("client")
+include("remote-lib")
